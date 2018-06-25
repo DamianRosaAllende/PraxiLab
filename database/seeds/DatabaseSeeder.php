b9fb1b9e08fa35793622de12460fa98708c8247f
Unknown <edu.r994@gmail.com>
2018-06-24 23:23:25 -0300

    Test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {	
+
     	$this->call(rubrosSeeder::class);
         $this->call(serviciosSeeder::class);
+        
     }
 }
